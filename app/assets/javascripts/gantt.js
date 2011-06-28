49fb67eac9dc66bbb200832939d8218a5a611ec5
Jay Phillips <jicksta+git@gmail.com>
2011-06-27 21:44:20 -0700

    Render the correct number of weeks. Misc aesthetic improvements.

--- app/assets/javascripts/gantt.js
+++ app/assets/javascripts/gantt.js
@@ -2,7 +2,7 @@ var Gantt = {};
 
 (function(ns) {
 
-  var NOW = Date.parse("Mon, 27 Jun 2011 19:35:16 -0700");
+  var NOW = new Date("Mon, 27 Jun 2011 19:35:16 -0700");
 
   var projects = _([
     {"name": "engineer world-class web-readiness", "start": "Mon, 02 May 2011 19:35:16 -0700", "end": "Mon, 16 May 2011 19:35:16 -0700"},
@@ -21,24 +21,27 @@ var Gantt = {};
 
 
   function newInit(projects) {
-    var firstTime = new Date(projects.min(
-        function(project) {
-          return new Date(project.start);
-        }).start);
-    var lastTime = new Date(projects.max(
-        function(project) {
-          return new Date(project.end);
-        }).end);
 
-    var startWeek = firstTime.getWeek();
-    var numberOfWeeks = lastTime.getWeek() - startWeek;
+    var allDateStrings = projects.pluck("start").concat(projects.pluck("end"));
+    var allDates = _.map(allDateStrings, function(dateString) {
+      return new Date(dateString);
+    });
+
+    var earliestDate = _.min(allDates, function(date) {
+      return Math.min(NOW, date);
+    });
+    var latestDate   = _.max(allDates, function(date) {
+      return Math.max(NOW, date);
+    });
+
+    var numberOfWeeks = 1 + latestDate.getWeek() - earliestDate.getWeek();
 
     var weekWidthPercentage = 100 / numberOfWeeks;
     var dayWidthPercentage = weekWidthPercentage / 7;
 
     var weeksContainer = $(".weeks");
     for (var i = 0; i < numberOfWeeks; i++) {
-      $("<div/>").addClass("week").text(i).appendTo(weeksContainer).css("width", weekWidthPercentage + "%");
+      $("<div/>").addClass("week").text(i).appendTo(weeksContainer).css("width", weekWidthPercentage + "%").addClass((i % 2 == 0) ? "even" : "odd");
     }
 
     var projectsContainer = $(".projects");
@@ -51,12 +54,18 @@ var Gantt = {};
       var lengthOfTimeInWeeks = end.getWeek() - start.getWeek();
 
       projectsContainer.append(projectView);
-      console.log(weekWidthPercentage * lengthOfTimeInWeeks);
       projectView.css({
         width: (weekWidthPercentage * lengthOfTimeInWeeks) + "%",
-        "margin-left": ((start.getWeek() - startWeek) * weekWidthPercentage) + (start.getDay() * dayWidthPercentage) + "%"
+        "left": leftOffset(start)
       });
     });
+
+    var nowMarker = $(".vertical-line");
+    nowMarker.css("left", leftOffset(NOW));
+
+    function leftOffset(date) {
+      return ((date.getWeek() - earliestDate.getWeek()) * weekWidthPercentage) + (date.getDay() * dayWidthPercentage) + "%";
+    }
 
   }
 
